4ef314d2b4532da5fac63142b19a92b3bf9f184e
Grayson <grayson.waters@umontana.umt.edu>
2024-04-22 11:52:32 -0600

    added methods to MantaRay

--- src/main/java/com/example/project/Characters/MantaRay.java
+++ src/main/java/com/example/project/Characters/MantaRay.java
@@ -1,10 +1,31 @@
 package com.example.project.Characters;
 
 import com.example.project.Cultures.FishFolk;
-
+import java.util.Random;
 public class MantaRay implements FishFolk {
-
-    public boolean attemptNegotiation{
-        return True;
+    Random rand = new Random();
+    int diplomacyModifier = 10;
+    int aggressionModifier = 3;
+    public boolean attemptNegotiation(){
+        if (rand.nextInt(20)<diplomacyModifier){
+            return true;
+        }
+        return false;
     }
+        
+    public boolean tryToPickAFight() {
+        if (rand.nextInt(20) <= aggressionModifier) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean flee() {
+        if (rand.nextInt(20) > aggressionModifier+ diplomacyModifier) {
+            return true;
+        }
+        return false;
+    }
+
+
 }

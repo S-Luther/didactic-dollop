ab70c17289884aa3ffd00ed302645c62c3830ee3
McRodoku <126914671+McRodoku@users.noreply.github.com>
2024-04-22 11:43:32 -0600

    Update CatWarrior.java

--- src/main/java/com/example/project/Cultures/CatWarrior.java
+++ src/main/java/com/example/project/Cultures/CatWarrior.java
@@ -1,6 +1,6 @@
 package com.example.project.Cultures;
 
-public interface Klingon extends Culture {
+public interface CatWarrior extends Culture {
     int diplomacyModifier = 0;
     int fightiness = 10;
 

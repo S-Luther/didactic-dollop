f1f295441309d8da2ff3a57b5e150d93c871253b
pdstarz01 <elitesorenea1@gmail.com>
2024-04-24 11:29:38 -0600

    added a gitignore

---
+++ src/main/java/com/example/project/TestInteraction.java
@@ -0,0 +1,11 @@
+package com.example.project;
+
+public class TestInteraction {
+    public static void main(String[] args) {
+        System.out.println(SomeRandomCode());
+    }
+
+    public static String SomeRandomCode(){
+        return "Some Random Code";
+    }
+}

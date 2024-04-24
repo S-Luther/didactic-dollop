dcea1c348bcc11bce360891b2f5f9695de496196
Grayson <grayson.waters@umontana.umt.edu>
2024-04-24 11:45:17 -0600

    added shark species

---
+++ src/main/java/com/example/project/Species/Shark.java
@@ -0,0 +1,12 @@
+public class Shark extends FishFolk {
+    private String name;
+    private int age;
+    private String[] friends;
+    
+    // Constructor
+    public Shark(String name, int age, String[] friends) {
+        this.name = name;
+        this.age = age;
+        this.friends = friends;
+    }
+}
